d6baf4246f293b384843889c2260dcc4232b74d4
Lasse Heitgres <hello@f-io.dev>
2025-05-10 14:54:08 +0000

    test

--- src/web/index.ts
+++
@@ -1,6 +0,0 @@
-import CarplayWeb from './CarplayWeb.js'
-
-export * from '../modules/index.js'
-export { default as WebMicrophone } from './WebMicrophone.js'
-export * from './CarplayWeb.js'
-export default CarplayWeb
